cfad5c10e71043fd9e6939a1b59854462f281463
Lukas Klipp <klipp.lukas@gmail.com>
2024-07-12 10:54:56 +0200

    final

---
+++ js-array-methods_for-each/js/index.js
@@ -0,0 +1,28 @@
+const colors = [
+  "#343434",
+  "#7FB5B5",
+  "#633A34",
+  "#354D73",
+  "#F3A505",
+  "#2A6478",
+  "#CC0605",
+  "#497E76",
+  "#D36E70",
+  "#FF2301",
+  "#4C514A",
+  "#2E3A23",
+  "#5D9B9B",
+  "#474A51",
+  "#EFA94A",
+  "#4D5645",
+  "#606E8C",
+  "#A5A5A5",
+];
+
+colors.forEach((color) => {
+  const newDiv = document.createElement("div");
+  newDiv.style.width = "50px";
+  newDiv.style.height = "50px";
+  newDiv.style.background = color;
+  document.body.append(newDiv);
+});
