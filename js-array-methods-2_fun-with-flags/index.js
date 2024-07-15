7ea04ab60936072d6d9182dd321b1357b26b0899
Lukas Klipp <klipp.lukas@gmail.com>
2024-07-15 12:37:33 +0200

    final

---
+++ js-array-methods-2_fun-with-flags/index.js
@@ -0,0 +1,23 @@
+import { countries } from "./utils/countries.js";
+import { Country } from "./components/Country/Country.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+const queryInput = document.querySelector('[data-js="query-input"]');
+
+queryInput.addEventListener("input", (event) => {
+  container.innerHTML = "";
+
+  const searchString = event.target.value.toLowerCase();
+
+  const foundCountries = countries.filter((country) => {
+    return country.name.toLowerCase().startsWith(searchString);
+  });
+
+  if (foundCountries) {
+    foundCountries.forEach((country) => {
+      const countryElement = Country(country);
+      container.append(countryElement);
+    })
+    
+  }
+});
