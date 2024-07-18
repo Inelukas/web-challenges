51860b833e286926217a2a3297adf9da7d0caa14
Lukas Klipp <klipp.lukas@gmail.com>
2024-07-18 10:58:20 +0200

    final

---
+++ react-basics_hello-world-article/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+
+function HelloWorldArticle() {
+  return <article>
+    <h1>My title</h1>
+    <p>
+      Claw your carpet in places everyone can see - why hide my amazing artistic
+      clawing skills? cats are cute yet soft kitty warm kitty little ball of
+      furr i shredded your linens for you. Kitten is playing with dead mouse
+      kitty run to human with blood on mouth from frenzied attack on poor
+      innocent mouse, dont i look cute? flex claws on the humans belly and
+      purr like a lawnmower find a way to fit in tiny box or milk the cow or
+      nyan fluffness ahh cucumber!
+    </p>
+  </article>;
+}
+
+export default function App() {
+  return HelloWorldArticle();
+}
