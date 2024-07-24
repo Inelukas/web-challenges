54e18f64874202193de52f0b621f04b3a9264733
Lukas Klipp <klipp.lukas@gmail.com>
2024-07-24 13:25:56 +0200

    final

---
+++ react-state-3_journal-app-favorite-button-shared-state/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./App.css";
+import EntriesSection from "./components/EntriesSection";
+import EntryForm from "./components/EntryForm";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { uid } from "uid";
+
+const initialEntries = [
+  {
+    id: 1000,
+    isFavorite: false,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    isFavorite: false,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    isFavorite: false,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    isFavorite: false,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: Yay!",
+  },
+];
+
+function App() {
+  const [entries, setEntries] = useState(initialEntries);
+
+  function handleAddEntry(newEntry) {
+    const date = new Date().toLocaleDateString("en-us", {
+      dateStyle: "medium",
+    });
+    setEntries([
+      { id: uid(), isFavorite: false, date, ...newEntry },
+      ...entries,
+    ]);
+  }
+
+  function handleToggleFavorite(id) {
+    setEntries(
+      entries.map((entry) =>
+        id === entry.id ? entry.isFavorite === !isFavorite : entry
+      )
+    );
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="app__main">
+        <EntryForm onAddEntry={handleAddEntry} />
+        <EntriesSection entries={entries} />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
