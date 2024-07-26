197d6b32dfebc4bf40a10289e7fcce3ea4a82e9c
Lukas Klipp <klipp.lukas@gmail.com>
2024-07-26 11:06:40 +0200

    final

---
+++ react-styled-components-1_styled-button-cra/src/App.js
@@ -0,0 +1,14 @@
+import StyledButton from "./components/StyledButton";
+import PrimaryButton from "./components/PrimaryButton";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello from React</h1>
+      <StyledButton>Default Button </StyledButton>
+      <PrimaryButton>Second Button </PrimaryButton>
+    </main>
+  );
+}
+
+export default App;
---
+++ react-styled-components-1_styled-button-cra/src/components/PrimaryButton.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import StyledButton from "./StyledButton";
+
+const PrimaryButton = styled(StyledButton)`
+  background-color: red;
+  border: 5px solid black;
+`;
+
+export default PrimaryButton;
